ca34a1e899c3b2442c26c341e4e63190e15b6339
ilknur <ilknurkoparir262@gmail.com>
2025-12-17 17:11:56 +0300

    6-DOF-RoboticArm-ROS2-moveit2

---
+++ my_robot_arm_description/launch/robot_arm.launch.py
@@ -0,0 +1,39 @@
+from launch import LaunchDescription
+import os
+from ament_index_python.packages import get_package_share_path
+from launch_ros.parameter_descriptions import ParameterValue
+from launch.substitutions import Command
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import PathJoinSubstitution
+
+def generate_launch_description():
+    urdf_path = os.path.join(get_package_share_path('my_robot_arm_description'),'urdf','my_robot.urdf.xacro')
+    robot_description = ParameterValue(Command(['xacro ', urdf_path]), value_type=str)
+
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[{'robot_description': robot_description}]
+    )
+ 
+    joint_state_publisher_node =Node(
+        package="joint_state_publisher_gui",
+        executable="joint_state_publisher_gui"
+    )
+
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        arguments=['-d',os.path.join(get_package_share_path('my_robot_arm_description'),'rviz','config.rviz')]
+    )
+
+
+    return LaunchDescription([
+        robot_state_publisher_node,
+        joint_state_publisher_node,
+        rviz_node
+
+    ])
